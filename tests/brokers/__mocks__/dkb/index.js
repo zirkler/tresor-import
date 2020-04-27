c238520d6867bb7ca8674d8196113dd8db993433
Dan <dan@imake.io>
2020-04-27 21:14:54 +0200

    add more DKB tests

---
+++ tests/brokers/__mocks__/dkb/index.js
@@ -0,0 +1,14 @@
+export const buySamples = [
+    require('./buy-1.json'),
+    require('./buy-2.json'),
+    require('./buy-3.json'),
+];
+
+export const sellSamples  = [
+    require('./sell-1.json'),
+];
+
+export const dividendsSamples  = [
+    require('./div-1.json'),
+    require('./div-2.json'),
+];
